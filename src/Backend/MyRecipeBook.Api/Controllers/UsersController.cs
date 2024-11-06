0c3cf8fc83a47459b7b6e3ca94ae3668049d8eaa
Rafael Moreira da Silva <rafaelmoreira.silva22@gmail.com>
2024-11-06 20:45:54 -0300

    feature: adding to endpoint to register an user

---
+++ src/Backend/MyRecipeBook.Api/Controllers/UsersController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyRecipeBook.Communication.Requests;
+
+namespace MyRecipeBook.Api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpPost]
+        [ProducesResponseType(typeof(RegisterUserResponse), StatusCodes.Status201Created)]
+        public IActionResult Register(RegisterUserRequest request)
+        {
+            return Created();
+        }
+    }
+}
---
+++ src/Shared/MyRecipeBook.Communication/Requests/RegisterUserResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace MyRecipeBook.Communication.Requests
+{
+    public class RegisterUserResponse
+    {
+        public string Nome { get; set; } = string.Empty;
+    }
+}
